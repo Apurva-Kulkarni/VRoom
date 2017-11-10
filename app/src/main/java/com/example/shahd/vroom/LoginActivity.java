b64412ef41242cd8ea1a40ccc38f7c9b628533b4
shrav13 <shravani.agarkar@gmail.com>
2017-11-10 13:11:51 +0530

    shravani

---
+++ app/src/main/java/com/example/shahd/vroom/LoginActivity.java
@@ -0,0 +1,38 @@
+package com.example.shahd.vroom;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_login);
+        final EditText etUsername=(EditText) findViewById(R.id.etUsername);
+        final EditText etPassword=(EditText) findViewById(R.id.etPassword);
+        final Button etLogin= (Button) findViewById(R.id.bLogin);
+
+        final TextView RegisterLink= (TextView) findViewById(R.id.tvRegisterHere);
+        RegisterLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+                LoginActivity.this.startActivity(registerIntent);
+            }
+        });
+
+    }
+
+    public void buttonClick(View view) {
+        Intent intent= new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
+    }
+}
